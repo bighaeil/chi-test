a54fccb46a5a1b5d81ced6f82e9ee746dfae5b54
5004 <5004>
2017-07-17 17:44:51 +0900

    update prepend
    스크롤바 앞으로 가면 그림 추가

--- src/js/main.js
+++ src/js/main.js
@@ -5,8 +5,26 @@ var wWidth = $(window).width();
 var imgItems = 2;
 var imgW = 500;
 
+$(window).ready(function() {
+    $(document).scrollLeft(imgItems * imgW - wWidth);
+});
+
 $(window).scroll(function() {
 
+    if(!$(document).scrollLeft()) {
+        console.log('scroll Left');
+        imgItems++;
+
+        var liHtml = '';
+
+        liHtml += '<li ' + 'id="' + imgItems + '">';
+        liHtml += '<img src="./img/singapore.jpg">';
+        liHtml += '</li>';
+
+        $('.test-ul').prepend(liHtml);
+    }
+
+    /*
     console.log($(document).scrollLeft() + ' ' + imgItems);
     if(imgW * imgItems === $(document).scrollLeft() + wWidth) {
         console.log('scroll End');
@@ -20,5 +38,6 @@ $(window).scroll(function() {
 
         $('.test-ul').append(liHtml);
     }
+    */
 
 });
