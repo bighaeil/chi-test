246e08d4274be22c12e678bada263e14b0cf951e
5004 <5004>
2017-07-21 17:49:53 +0900

    model 정리 처음 4개 오른쪽 붙으면 4개 추가

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,32 @@
 require('bootstrap');
 require('../less/main.less');
 
-var wWidth = $(window).width();
-var imgItems = 2;
-var imgW = 500;
+var WINDOW_WIDTH = $(window).width();
+var START_ITEMS = 4;
+var ATTACH_TIEMS = 4;
+var TOTAL_ITEMS = START_ITEMS;
+
+var IMG_WIDTH = 500;
 
 $(window).ready(function() {
-    $(document).scrollLeft(imgItems * imgW - wWidth);
+    // 시작시 스크롤 오른쪽 붙이기
+    // $(document).scrollLeft(imgItems * imgW - wWidth);
+    var testTemplate = require('../template/chi-model.hbs');
+    var models = require('./model/model1');
+    var testHtml = testTemplate(models);
+
+    $('.test-ul').append(testHtml);
+});
+
+$(window).resize(function () {
+    WINDOW_WIDTH = $(window).width()
+    console.log('resize: ' + WINDOW_WIDTH);
 });
 
 $(window).scroll(function() {
 
+    /*
+    // 왼쪽에 붙이기
     if(!$(document).scrollLeft()) {
         console.log('scroll Left');
         imgItems++;
@@ -23,25 +39,29 @@ $(window).scroll(function() {
 
         $('.test-ul').prepend(liHtml);
     }
-
-    /*
-    console.log($(document).scrollLeft() + ' ' + imgItems);
-    if(imgW * imgItems === $(document).scrollLeft() + wWidth) {
-        console.log('scroll End');
-        imgItems++;
-
-        var liHtml = '';
-
-        liHtml += '<li ' + 'id="' + imgItems + '">';
-        liHtml += '<img src="./img/singapore.jpg">';
-        liHtml += '</li>';
-
-        $('.test-ul').append(liHtml);
-    }
     */
 
+    // 오른쪽에 붙이기
+    if(IMG_WIDTH * TOTAL_ITEMS <= $(document).scrollLeft() + WINDOW_WIDTH) {
+        console.log('scroll End');
+        scrollRightEnd();
+    }
 });
 
+function scrollRightEnd() {
+    TOTAL_ITEMS += ATTACH_TIEMS;
+
+    // 필요 - 입력되는 그림이 window size보다 작으면 pass, 크면 size로 줄임
+    // 필요? - 글자수 넘어가면 ... 대체
+
+    var testTemplate = require('../template/chi-model.hbs');
+    var models = require('./model/model1');
+    var testHtml = testTemplate(models);
+
+    $('.test-ul').append(testHtml);
+}
+
+/*
 $(document).on('touchstart', function(e) {
     $('#msg').html('터치가 시작되었어요.');
     e.preventDefault();	//	이벤트취소
@@ -59,3 +79,4 @@ $(document).on('touchmove', function(e) {
 $(document).on('touchend', function(e) {
     $('#msg').append("<div>터치이벤트가 종료되었어요</div>");
 });
+*/
---
+++ src/js/model/model1.js
@@ -0,0 +1,17 @@
+module.exports = [{
+    title: '제목1',
+    img: './img/sentosa.jpg',
+    content: '내용1<br>내용2'
+},{
+    title: '제목2',
+    img: './img/sentosa2.jpg',
+    content: '내용1<br>내용2'
+},{
+    title: '제목3',
+    img: './img/singapore.jpg',
+    content: '내용1<br>내용2'
+},{
+    title: '제목4',
+    img: './img/singapore2.jpg',
+    content: '내용1<br>내용2'
+}];
